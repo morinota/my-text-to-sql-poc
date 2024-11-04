36b434cf7fa714fcd7cdcbb4d10ac4ee9a39e7ca
morita <morinota416@gmail.com>
2024-11-04 15:23:30 +0900

    Implement table schema selection with error handling and available tables display

--- src/my_text_to_sql_poc/__main__.py
+++ src/my_text_to_sql_poc/__main__.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 import openai
 import typer
@@ -7,7 +8,7 @@ from loguru import logger
 
 from my_text_to_sql_poc.sql_formatter import format_sql_query
 
-app = typer.Typer()  # Typerアプリケーションのインスタンスを作成
+app = typer.Typer(pretty_exceptions_enable=False)  # Typerアプリケーションのインスタンスを作成
 
 # 環境変数からOpenAI APIキーを設定
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -17,7 +18,7 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
 
 # プロンプトをファイルから読み込む関数
-def load_prompt(file_path="prompts/generate_sql_prompt.txt"):
+def load_prompt(file_path: str = "prompts/generate_sql_prompt.txt") -> str:
     try:
         with open(file_path, "r") as file:
             prompt = file.read()
@@ -28,24 +29,32 @@ def load_prompt(file_path="prompts/generate_sql_prompt.txt"):
         raise
 
 
-# テーブルスキーマをJSONファイルから読み込む関数
-def load_table_schemas(file_path="schema/tables_schema.json"):
-    try:
-        with open(file_path, "r") as file:
-            schema_data = json.load(file)
-            logger.debug(f"Loaded table schemas from {file_path}")
-            return schema_data
-    except Exception as e:
-        logger.error(f"Failed to load table schemas from {file_path}: {e}")
-        raise
-
-
-# スキーマ情報をフォーマットする関数
-def format_table_schemas(schema_data):
+def load_selected_table_schemas(table_names: list[str] = []) -> str:
+    """テーブルスキーマを読み込んでフォーマット"""
     formatted_schemas = ""
-    for table in schema_data["tables"]:
-        formatted_schemas += f"Table: {table['name']}\nColumns: "
-        formatted_schemas += ", ".join([col["name"] for col in table["columns"]]) + "\n\n"
+    schema_dir = Path("schema/")
+    available_tables = [file.stem for file in schema_dir.glob("*.json")]
+
+    # 指定されたテーブルがなければ全てのテーブルスキーマを取得
+    if not table_names:
+        table_names = available_tables.copy()
+
+    for table_name in table_names:
+        file_path = schema_dir / f"{table_name}.json"
+        if not file_path.exists():
+            raise FileNotFoundError(
+                f"Schema file not found for table: {table_name}\n Available tables: {available_tables}"
+            )
+
+        try:
+            with file_path.open("r") as file:
+                table_data = json.load(file)
+                formatted_schemas += f"Table: {table_data['table_name']}\nColumns: "
+                formatted_schemas += ", ".join([col["name"] for col in table_data["columns"]]) + "\n\n"
+                logger.debug(f"Loaded schema for table: {table_name}")
+        except Exception as e:
+            logger.error(f"Error loading schema for table {table_name}: {e}")
+            raise e
     return formatted_schemas
 
 
@@ -97,9 +106,10 @@ def generate_sql_query(
 @app.command()
 def main(
     question: str = typer.Option(..., help="The user's question (natural language)"),
+    tables: list[str] = typer.Option([], "-T", "-table", help="List of tables to use", show_default=False),
     dialect: str = typer.Option("SQLite", help="The SQL dialect to use (default is SQLite)"),
     log_level: str = typer.Option("INFO", help="Set the logging level (DEBUG, INFO, WARNING, ERROR)"),
-):
+) -> None:
     """
     自然言語の質問からSQLクエリを生成します。
 
@@ -113,8 +123,7 @@ def main(
     logger.add(lambda msg: typer.echo(msg, err=True), level=log_level.upper())
 
     # テーブルスキーマをロードしてフォーマット
-    schema_data = load_table_schemas()
-    table_schemas = format_table_schemas(schema_data)
+    table_schemas = load_selected_table_schemas(tables)
 
     sql_query, explanation = generate_sql_query(dialect, question, table_schemas)
     # SQLクエリ部分の改行コードを変換
