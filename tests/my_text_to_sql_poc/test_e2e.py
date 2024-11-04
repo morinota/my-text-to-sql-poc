88a7d6e3e0008b2e88462ff61b2363016c9def95
morita <morinota416@gmail.com>
2024-11-04 13:18:20 +0900

    Test: Add TODO for examining the use of test doubles in E2E tests

--- tests/my_text_to_sql_poc/test_e2e.py
+++ tests/my_text_to_sql_poc/test_e2e.py
@@ -1,11 +1,42 @@
+import json
+
+from pytest_mock import MockerFixture
 from typer.testing import CliRunner
+
 from my_text_to_sql_poc.__main__ import app
 
 runner = CliRunner()
 
 
-def test_cli_e2e():
-    # CLIコマンドをシミュレートして実行
+def test_cli_e2e(mocker: MockerFixture) -> None:
+    # Arrange
+    # TODO: E2Eテストなのに、テストダブルで置き換えるべきかは要検討
+    ## OpenAI APIとのやりとりのみ、テストダブルに置き換える
+    test_double_response = {
+        "choices": [
+            {
+                "message": {
+                    "content": json.dumps(
+                        {
+                            "query": "SELECT * FROM sales",
+                            "explanation": "Test explanation",
+                        }
+                    )
+                }
+            }
+        ],
+        "usage": {
+            "prompt_tokens": 10,
+            "completion_tokens": 20,
+            "total_tokens": 30,
+        },
+    }
+    mocker.patch(
+        "openai.ChatCompletion.create",
+        return_value=test_double_response,
+    )
+
+    # Act
     result = runner.invoke(
         app,
         [
@@ -18,9 +49,9 @@ def test_cli_e2e():
         ],
     )
 
-    # コマンドが成功して終了したことを確認
+    # Assert
+    ## コマンドが成功して終了したことを確認
     assert result.exit_code == 0, f"CLI exited with non-zero status: {result.exit_code}"
-
-    # 標準出力の内容を確認
+    ## 標準出力の内容を確認
     assert "Generated SQL Query:" in result.output
-    assert "SELECT * FROM sales WHERE amount > 100;" in result.output
+    assert "Explanation:" in result.output
