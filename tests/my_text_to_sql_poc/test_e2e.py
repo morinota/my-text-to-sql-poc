a2354c604a1ea2699361e084261c4fdb033dc4f5
morita <morinota416@gmail.com>
2024-11-13 18:25:14 +0900

    E2Eテストでモデル呼び出しをテストダブルにおきかえるのをやめた

--- tests/my_text_to_sql_poc/test_e2e.py
+++ tests/my_text_to_sql_poc/test_e2e.py
@@ -6,22 +6,7 @@ from my_text_to_sql_poc.__main__ import app
 runner = CliRunner()
 
 
-def test_cli_e2e(mocker: MockerFixture) -> None:
-    # Arrange
-    # TODO: E2Eテストなのに、テストダブルで置き換えるべきかは要検討
-    ## Model Gatewayのgenerate_responseメソッドをテストダブルに置き換える
-    test_double_response = """
-    {
-        "query": "The SQL query is: SELECT SUM(sales) FROM sales_table WHERE year = 2023.",
-        "explanation": "This query calculates the total sales from the sales_table where the year is 2023."
-    }
-    """
-
-    mocker.patch(
-        "my_text_to_sql_poc.service.model_gateway.ModelGateway.generate_response",
-        return_value=test_double_response,
-    )
-
+def test_cli_e2e() -> None:
     # Act
     result = runner.invoke(
         app,
