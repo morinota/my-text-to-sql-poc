2606c6d7a4b4cf0de01a95934126a4cea2e5698b
morita <morinota416@gmail.com>
2024-11-18 22:03:48 +0900

    Text2SQL appのver1を削除して、ver2のディレクトリ名を変更した!

--- tests/my_text_to_sql_poc/app/generate_sql_query/test_e2e_generate_sql_query_ver2.py
+++ tests/my_text_to_sql_poc/app/generate_sql_query/test_e2e_generate_sql_query_ver2.py
@@ -1,11 +1,11 @@
 from typer.testing import CliRunner
 
-from my_text_to_sql_poc.app.generate_sql_query_ver1.__main__ import app
+from my_text_to_sql_poc.app.generate_sql_query.__main__ import app
 
 runner = CliRunner()
 
 
-def test_cli_e2e() -> None:
+def test_generate_sql_query_with_context_construction() -> None:
     # Act
     result = runner.invoke(
         app,
@@ -14,8 +14,6 @@ def test_cli_e2e() -> None:
             "2023年の売上合計は？",
             "--dialect",
             "SQLite",
-            "--log-level",
-            "DEBUG",
         ],
     )
 
@@ -23,4 +21,5 @@ def test_cli_e2e() -> None:
     ## コマンドが成功して終了したことを確認
     assert result.exit_code == 0
     ## 標準出力の内容を確認
-    assert "select sum(sales) from sales where year = 2023" in result.output
+    assert "Generated SQL Query:" in result.output
+    assert "Explanation:" in result.output
